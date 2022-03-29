4c7c8c78aaa89b518fbaf12f34c1e85eda84210e
harmolipi <nikob381@gmail.com>
2022-03-29 17:15:28 -0400

    Filled out conversion formulae

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,56 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
+  this.getNum = function (input) {
     let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.getUnit = function (input) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    let result;
+
+    return result;
+  };
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
+    if (initUnit === 'gal') {
+      result = initNum * galToL;
+    } else if (initUnit === 'l') {
+      result = initNum / galToL;
+    } else if (initUnit === 'lbs') {
+      result = initNum * lbsToKg;
+    } else if (initUnit === 'kg') {
+      result = initNum / lbsToKg;
+    } else if (initUnit === 'mi') {
+      result = initNum * miToKm;
+    } else if (initUnit === 'km') {
+      result = initNum / miToKm;
+    }
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
