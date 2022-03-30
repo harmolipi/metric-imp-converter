b995a6b1e02b349c577b4fc60b3ae9b60759eaf8
harmolipi <nikob381@gmail.com>
2022-03-30 09:53:51 -0400

    Added conversion routes

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,24 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
+  app.route('/api/convert').get((req, res) => {
+    const input = req.query.input;
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const toString = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
 
+    if (initNum === false || initUnit === false) {
+      res.json({ error: 'invalid number or unit' });
+    } else {
+      res.json({ initNum, initUnit, returnNum, returnUnit, toString });
+    }
+  });
 };
